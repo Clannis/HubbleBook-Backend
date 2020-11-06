4af8b803998a7294ebb41faa6ec362d752274fc6
Clannis <Dodds1990@gmail.com>
2020-11-05 20:19:14 -0600

    change CORS to heroku app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hubblebook.herokuapp.com'
+    origins 'hubblebook.herokuapp.com'
 
     resource '*',
       headers: :any,

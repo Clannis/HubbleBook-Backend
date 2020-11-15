cff6aae2713ee12d518dfe6a796dbfbb87c99dec
Clannis <Dodds1990@gmail.com>
2020-11-15 10:02:40 -0600

    add route for mission selection, add show and mission controller functions, add acticle method to return articles array based on mission

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,15 @@ class ArticlesController < ApplicationController
         articles = Article.all.sort_by { |key| key[:publication]}.reverse
         render json: articles
     end
+
+    def show
+        article = Article.find_by(news_id: params[:id])
+        render json: article
+    end
+
+    def missions
+        puts(params[:mission_name])
+        articles = Article.find_by_mission(params[:mission_name])
+        render json: articles
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+
+    def find_by_mission(mission)
+        return Article.where(mission: mission)
+    end
+    
 end

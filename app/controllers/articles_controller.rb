5aa3ee3d052d92883f35900e5473cf1d7a5a18f6
Clannis <Dodds1990@gmail.com>
2020-11-15 08:49:30 -0600

    return sorted articles by publication date on index request

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
     def index
-        articles = Article.all
+        articles = Article.all.sort_by { |key| key[:publication]}.reverse
         render json: articles
     end
 end

ad1d0caae6c4a54a355545d89a1aa8f65eee6b71
Clannis <Dodds1990@gmail.com>
2020-11-23 12:52:08 -0600

    add secure password to user and create route for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: user
+        else
+          render json: {errors: user.errors.full_messages.to_sentence}
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:email, :username, :password)
+    end
+
 end

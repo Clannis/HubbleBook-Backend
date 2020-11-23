84f28d74f1b7f8fc713a26e91452a0c18a75bcc3
Clannis <Dodds1990@gmail.com>
2020-11-23 12:26:36 -0600

    generate users resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_142524) do
+ActiveRecord::Schema.define(version: 2020_11_23_182533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2020_11_01_142524) do
     t.string "thumbnail_2x"
     t.string "keystone_image_1x"
     t.string "keystone_image_2x"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

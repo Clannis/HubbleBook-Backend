932468dd45925865f92e897ef7a5096eca1647da
Clannis <Dodds1990@gmail.com>
2020-11-23 21:47:44 -0600

    add functionality to log in a user utilizing JWT tokens

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,23 @@
 class SessionsController < ApplicationController
+  
   def login
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+          token = encode_token(user.id)
+          render json: {user: UserSerializer.new(user), token: token}
+      else
+          render json: {errors: "Either your username or password was incorrect. Please Try again."}
+      end
   end
 
   def autologin
+      user = session_user
+      if user
+          token = encode_token(user.id)
+          render json: {user: UserSerializer.new(user), token: token}
+      else
+          render json: {errors: "Dont try and hack me. I see YOU!"}
+      end
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,10 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-          render json: user
+            token = encode_token(user.id)
+            render json: { user: UserSerializer.new(user), token: token }
         else
-          render json: {errors: user.errors.full_messages.to_sentence}
+            render json: { errors: user.errors.full_messages.to_sentence} 
         end
     end
 

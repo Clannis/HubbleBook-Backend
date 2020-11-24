d6c3eef865ce37349eedf998430b24e2b59febbe
Clannis <Dodds1990@gmail.com>
2020-11-23 23:16:11 -0600

    remove foreign key from comments and all associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, :email, presence: true,  uniqueness: { case_sensitive: false }
-    has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,10 +56,8 @@ ActiveRecord::Schema.define(version: 2020_11_24_040410) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -71,5 +69,4 @@ ActiveRecord::Schema.define(version: 2020_11_24_040410) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "users"
 end

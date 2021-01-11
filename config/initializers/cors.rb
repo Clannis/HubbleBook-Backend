80e230147eb4607b29021dc2312b777db73d1cdf
Clannis <dodds1990@gmail.com>
2021-01-11 16:11:30 -0600

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :delete],
-      credentials: true
+      methods: [:get, :post, :put, :delete]
   end
 end
